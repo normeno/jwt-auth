88c51aec553933103e879670993c6acc6a000745
Nicolas Ormeno <ni.ormeno@gmail.com>
2020-07-04 10:57:14 -0400

    Add Version

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,10 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const VERSION = process.env.APP_VERSION || '';
 
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+app.get('/version', (req, res) => {
+  res.send(`Version: ${VERSION}`);
 });
 
 app.listen(PORT, () => {
