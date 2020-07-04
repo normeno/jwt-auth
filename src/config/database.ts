e20d74ad7339e256750fbdaabd308f4dfadbf80a
Nicolas Ormeno <ni.ormeno@gmail.com>
2020-07-04 18:23:30 -0400

    Update database

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,10 +2,14 @@ import fs from 'fs';
 import lowdb from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
 import path from 'path';
+import uniqid from 'uniqid';
 import logger from '../logger';
+import { IDatabase } from '../db/interfaces';
 
-const DB_DIR_PATH = path.join(__dirname, '..', process.env.DB_DIR || 'db');
-const DB_FILE_PATH = path.join(DB_DIR_PATH, process.env.DB_FILE || 'db.json');
+require('dotenv').config();
+
+const DB_DIR_PATH: string = path.join(__dirname, '..', process.env.DB_DIR || 'db');
+const DB_FILE_PATH: string = path.join(DB_DIR_PATH, process.env.DB_FILE || 'db.json');
 
 if (!fs.existsSync(DB_DIR_PATH)) {
   logger.info('[DATABASE] Writing database directory');
@@ -14,7 +18,51 @@ if (!fs.existsSync(DB_DIR_PATH)) {
   logger.info('[DATABASE] database directory already exists');
 }
 
-const ADAPTER = new FileSync(DB_FILE_PATH);
-const DB = lowdb(ADAPTER);
+const ADAPTER = new FileSync<IDatabase>(DB_FILE_PATH);
+const db = lowdb(ADAPTER);
 
-export default DB;
+function updateCount() {
+  db.update('count', (n: number) => n + 1)
+    .write();
+}
+
+function getAdmin(username: string, password: string) {
+  return db.get('users')
+    .find({ username, password, type: 'admin' })
+    .value();
+}
+
+// Check if model exists
+const userExists: boolean = db.has('users').value();
+
+if (!userExists) {
+  db.set('users', []).write();
+}
+
+// Check if count exists
+const countExists: boolean = db.has('count').value();
+
+if (!countExists) {
+  db.set('count', 0).write();
+}
+
+// Default admin
+const admin = {
+  id: uniqid().toString(),
+  username: process.env.ADMIN_USERNAME,
+  password: process.env.ADMIN_PASSWORD,
+  type: process.env.ADMIN_TYPE,
+};
+
+// Check if admin exists
+const adminExists = db.get('users').find({ username: admin.username }).value();
+
+if (!adminExists) {
+  db.get('users')
+    .push(admin)
+    .write();
+
+  updateCount();
+}
+
+export { db, getAdmin };
